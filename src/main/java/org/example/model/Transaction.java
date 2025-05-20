570f76a0a027baadc5b49dbde160ad386e483a3e
Guilherme <gui.mfrancoi@gmail.com>
2025-05-19 23:05:32 -0300

    Adicionado dependencias e começo da adição das tabelas

--- src/main/java/org/example/model/Transaction.java
+++ src/main/java/org/example/model/Transaction.java
@@ -1,13 +1,22 @@
 package org.example.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String date;
     private String category;
     private String description;
     private double value;
     private String type;
 
-    public Transaction(String date, String category, String description, double value, String type) {
+    public Transaction(Long id, String date, String category, String description, double value, String type) {
+        this.id = id;
         this.date = date;
         this.category = category;
         this.description = description;
@@ -15,7 +24,16 @@ public class Transaction {
         this.type = type;
     }
 
+    public Transaction() {
+
+    }
+
     // Getters
+
+    public Long getId() {
+        return id;
+    }
+
     public String getDate() { return date; }
     public String getCategory() { return category; }
     public String getDescription() { return description; }
